f73f68ecc327abc440b452629709a6092cf47c6a
Shivani Patel <shivani.patel63@outlook.com>
2023-09-03 22:43:06 +0100

    Add task 2 python

--- maxmin.py
+++ maxmin.py
@@ -1,25 +1,21 @@
-#given a LIST of integers, return the highest and lowest numbers without using max() or min()
-def input_integers():
-    integers =input('Please input a list of integers of your choice, separated by a comma:')
-    int_list = integers.split(',')
-    return int_list
-
-def find_min(int_list):
+def find_min_max(int_list):
     min_val = int_list[0]
+    max_val = int_list[0]
     for num in int_list:
         if num < min_val:
             min_val = num
-    return min_val
-
-def find_max(int_list):
-    max_val = int_list[0]
-    for num in int_list:
         if max_val < num:
             max_val = num
-    return max_val
-
-def min_and_max(min_val,max_val):
     ans = [int(min_val), int(max_val)]
     return ans
 
-print(min_and_max(find_min(input_integers()),find_max(input_integers())))
+test_cases =  [
+    [10,-10,-100,67,9],
+    [10,14,16,28],
+    [-67,-15,-75,-100]
+]
+for test in test_cases:
+    print(find_min_max(test))
+
+#some test case examples, to check the find_min_max() function works
+#expected output: [-100,67][10,28][-100,-15]
