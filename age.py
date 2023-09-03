5cb4fd8648c7a137b968464b551f4a7e95b328be
Shivani Patel <shivani.patel63@outlook.com>
2023-09-03 22:42:28 +0100

    Add task 1 python

--- age.py
+++ age.py
@@ -1,29 +1,26 @@
 from datetime import datetime
 import math
 
-def converting_user_input():
-    users_input = input('Please input a date in the format of YYYY/MM/DD:')
-    date = datetime.strptime(users_input, "%Y/%m/%d")
-    return date
-#this function is converting the users input into a datetime object
-
-def calculate_difference():
-    date1 = converting_user_input()
+def calculate_difference(date_string):
+    date1 = datetime.strptime(date_string, "%Y/%m/%d")
     date2 = datetime.now()
     difference = str(date2 - date1)
-    return difference 
+    return difference
 #this function is calculating the difference between the users input and current date
 
-def formatting_age():
-    list_diff = (calculate_difference().split(' '))
+def formatting_to_age(difference,date_string):
+    list_diff = (calculate_difference(date_string).split(' '))
     #splitting the difference value, as it gives difference in days, hours, minutes and seconds.
     days_int = int(list_diff[0])
-    #index reference to the days part only
     days_to_years = (days_int/365.25)
     #converting days to years
-    display_difference = print(f'The difference in years is {math.floor(days_to_years)}')
+    display_difference = (f'This birthdate ({date_string}) would make you {math.floor(days_to_years)} years old')
     return display_difference
 #this function is formatting the values found from the calculate_difference() function.
 
-formatting_age()
-#call to final function, which pulls the previous two functions into call as well.
+test_cases = ['1990/08/31', '2000/03/06', '2013/09/04']
+
+for test in test_cases:
+    difference = calculate_difference(test)
+    formatted_age = formatting_to_age(difference, test)
+    print(formatted_age)
